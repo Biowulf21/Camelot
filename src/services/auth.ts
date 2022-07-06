71a7a13d10780e4acc822177747621b4f0ebceef
James Jilhaney <20190016830@my.xu.edu.ph>
2022-07-06 11:37:27 +0800

    Added .env file

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, createUserWithEmailAndPassword, Auth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+
+// https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
-  apiKey: "AIzaSyCS9CWPUYf68eEo2GbNYyflWmiQto3rSO8",
-  authDomain: "camelot-59ea0.firebaseapp.com",
-  projectId: "camelot-59ea0",
-  storageBucket: "camelot-59ea0.appspot.com",
-  messagingSenderId: "970650568035",
-  appId: "1:970650568035:web:25ea362fdacd882f1a3f56"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGER_ID,
+  appId: process.env.REACT_APP_FIREBASE_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
