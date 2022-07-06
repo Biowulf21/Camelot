e1cc050353712dbb9ea2dc5b022d8d7b32d6017f
James Jilhaney <20190016830@my.xu.edu.ph>
2022-07-06 15:03:31 +0800

    Added error 404 page

---
+++ src/screens/authenticated/dashboard/sub-components/sidebar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Offcanvas } from 'react-bootstrap'
+
+const SideBar = () => {
+  return (
+    <Offcanvas show={true}></Offcanvas>
+  )
+}
+
+export default SideBar
