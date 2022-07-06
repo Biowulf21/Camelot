650c7cfaebe685780de8917e1103c9222e1f22d1
James Jilhaney <20190016830@my.xu.edu.ph>
2022-07-06 17:19:30 +0800

    Added styling to navbar

--- src/screens/authenticated/dashboard/sub-components/navbar/index.tsx
+++ src/screens/authenticated/dashboard/sub-components/navbar/index.tsx
@@ -4,7 +4,9 @@ import { useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faNavicon } from '@fortawesome/free-solid-svg-icons';
 import { getAuth, signOut } from 'firebase/auth';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import "./styles.css";
+import ProfileCard from './sub-components/profile-card';
 
 
 const SideBar = () => {
@@ -27,33 +29,39 @@ const SideBar = () => {
     <>
     { location.pathname === "/" ? null :
     <Navbar>
-      <Navbar.Offcanvas show={show} onClick={handleShow} onHide={handleClose }>
-      <Offcanvas.Header closeButton>
+      <Offcanvas show={show} onHide={handleClose} responsive="lg">
+        <Offcanvas.Header closeButton>
           <Offcanvas.Title>Camelot - The Crusader Yearbook</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <ul style={{listStyle: 'none'}}>
-            <li>
-                <a href='/merlin'>Merlin</a>
-            </li>
-            <li>
-                <a href='/arthur'>Arthur</a>
-            </li>
-            <li>
-                <a href='/lancelot'>Lancelot</a> 
-            </li>
-            <li>
-                <a href='/morgana'>Morgana</a>
-            </li>
-          </ul>
-          
+          <Nav>
+            <ProfileCard></ProfileCard>
+          </Nav>
+          <Nav className="nav-card">
+            <Link to={"/"}>Merlin</Link>
+          </Nav>
+          <Nav className="nav-card">
+            <Link to={"/"}>Arthur</Link>
+          </Nav>
+          <Nav className="nav-card">
+            <Link to={"/"}>Excalibur</Link>
+          </Nav>
+          <Nav className="nav-card">
+            <Link to={"/"}>Morgana</Link>
+          </Nav>
+          <Nav className="nav-card">
+            <Link to={"/"}>Settings</Link>
+          </Nav>
+          <Nav className="my-3">
+            <Button onClick={handleSignOut}>Logout</Button>
+          </Nav>
         </Offcanvas.Body>
-      </Navbar.Offcanvas>
+      </Offcanvas>
       <Button variant='outline-primary' className="mx-3" onClick={handleShow}><FontAwesomeIcon icon={faNavicon}></FontAwesomeIcon></Button>
       <Navbar.Brand href='/dashboard'>Camelot</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
-            <Button onClick={handleSignOut}>Logout</Button>
+            <Button className="mx-3" onClick={handleSignOut}>Logout</Button>
     </Navbar>
   }
     </>
