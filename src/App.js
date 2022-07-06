7c8306eff135a52fef04f5b0f0e9348e3c43d316
James Jilhaney <20190016830@my.xu.edu.ph>
2022-07-06 10:26:52 +0800

    Added firebase configuration

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./screens/guest/login/index";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <BrowserRouter>
       <Routes>
---
+++ src/services/auth.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCS9CWPUYf68eEo2GbNYyflWmiQto3rSO8",
+  authDomain: "camelot-59ea0.firebaseapp.com",
+  projectId: "camelot-59ea0",
+  storageBucket: "camelot-59ea0.appspot.com",
+  messagingSenderId: "970650568035",
+  appId: "1:970650568035:web:25ea362fdacd882f1a3f56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
