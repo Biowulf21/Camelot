5faabd8bb626adbe2e54532a5f13b79fbde55dce
James Jilhaney <20190016830@my.xu.edu.ph>
2022-07-06 14:42:44 +0800

    Logout feature added

--- src/screens/authenticated/dashboard/index.tsx
+++ src/screens/authenticated/dashboard/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { getAuth, signOut } from 'firebase/auth'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('Auth_Token')
+    if (!token){
+      navigate('/')
+    }
+  }, []);
+  
+  const handleSignOut = () =>{
+    const auth = getAuth()
+    signOut(auth)
+    sessionStorage.clear()
+    console.log('Deleted token')
+    console.log('Redirecting to login page')
+    navigate('/')
+  }
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h1>DASHBOARD</h1>
+    <Button onClick={handleSignOut}>Logout</Button>
+    </div>
   )
 }
 
