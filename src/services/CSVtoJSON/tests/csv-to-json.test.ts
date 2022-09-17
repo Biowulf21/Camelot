5b09e5c766c8e7341da8e7a48f73e73598d8b61e
James Jilhaney <notail2111@gmail.com>
2022-09-17 21:37:33 +0800

    changed unit test formatting for readability

--- src/services/CSVtoJSON/tests/csv-to-json.test.ts
+++ src/services/CSVtoJSON/tests/csv-to-json.test.ts
@@ -3,16 +3,22 @@ import {CSVtoJson} from "../csv-to-json-v2";
 test("Properly formats CSV to JSON objects.", () => {
     const converter = new CSVtoJson();
     // data key skips the last array element, as it is the headers of the CSV file
-    const positiveData = {data: [['email', 'name'],['james@example.com', 'James Jilhaney'], ['email', 'name']]};
+    const positiveData = {data: [['email', 'name'],['james@example.com', 'James Jilhaney'],
+    ['email', 'name']]};
     const result = converter.CSVtoJSON(positiveData);
 
-    expect(result).toStrictEqual({headers:["EMAIL","NAME"], body:[[{"EMAIL":"james@example.com","NAME":"James Jilhaney"}]],emailIndex:0})
+    expect(result).toStrictEqual({headers:["EMAIL","NAME"], 
+    body:[[{"EMAIL":"james@example.com","NAME":"James Jilhaney"}]],
+    emailIndex:0})
 })
 
 test("Throws error when no email column is found.", ()=>{
     const converter = new CSVtoJson();
-    const negativeData = {data: [['notemail', 'name'],['james@example.com', 'James Jilhaney'], ['oten1', 'oten1@gmail.com']]};
+    const negativeData = {data: [['notemail', 'name'],['james@example.com', 'James Jilhaney'], 
+    ['email', 'name']]};
     const result = converter.CSVtoJSON(negativeData);
 
-    expect(result).toStrictEqual({headers:["NOTEMAIL","NAME"], body:[[{"NOTEMAIL":"james@example.com","NAME":"James Jilhaney"}]],emailIndex:-1})
+    expect(result).toStrictEqual({headers:["NOTEMAIL","NAME"], 
+    body:[[{"NOTEMAIL":"james@example.com","NAME":"James Jilhaney"}]],
+    emailIndex:-1})
 })
