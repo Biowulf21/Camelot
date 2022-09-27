fbe6d2534901837efbc4e5bbc990a5ed24fedf45
James Jilhaney <notail2111@gmail.com>
2022-09-27 09:00:05 +0800

    Separated code for logic and UI

--- src/screens/authenticated/arthur/sub-components/subscriber-list/index.tsx
+++ src/screens/authenticated/arthur/sub-components/subscriber-list/index.tsx
@@ -1,10 +1,11 @@
-import { collection, doc, DocumentData, getDocs, limit, orderBy, query, serverTimestamp, setDoc, startAfter, where } from 'firebase/firestore'
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
-import { Button, Col, Container, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
+import { DocumentData } from 'firebase/firestore';
+import React, {  useEffect, useState } from 'react'
+import { Button, Col, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import { db } from '../../../../../services/firebase-config';
 import LoadingComponent from '../../../../global-components/loading-component';
 import './styles.css';
+import SubscriberListHook from './subscriberListHooks';
 
 interface subscriberListInterface{
   searchQuery: string;
@@ -12,152 +13,15 @@ interface subscriberListInterface{
 
 const SubscriberListComponent = (props:subscriberListInterface) => {
   const { searchQuery } = props;
-  const [displaySubsList, setdisplaySubsList] = useState<DocumentData[]>([]);
   const [isLoading, setisLoading] = useState(false);
-  const [lastDoc, setlastDoc] = useState<DocumentData>();
   const [noMoreSubs, setnoMoreSubs] = useState(false);
-  const [searchResultsList, setsearchResultsList] = useState<DocumentData[]>([]);
-  const [tempSubsList, settempSubsList] = useState<DocumentData[]>([]);
-  const [initialSubsList, setinitialSubsList] = useState<DocumentData[]>([]);
+  const [displaySubsList, setdisplaySubsList] = useState<DocumentData[]>([]);
 
 
-  useEffect(() => {
-    fetchSubscribers();
-  }, []);
-
-  useEffect(() => {
-    console.log('search: ');
-    console.log(searchResultsList);
-    setdisplaySubsList(searchResultsList);
-  }, [searchResultsList]);
-
-  useEffect(() => {
-    console.log('subsList: ')
-    console.log(displaySubsList);
-    console.log('temp list: ')
-    console.log(tempSubsList);
-  },[displaySubsList, tempSubsList]);
-
-  useEffect(() => {
-    console.log('query is: ' + searchQuery);
-
-    if (searchQuery === ""){
-      setdisplaySubsList(initialSubsList);
-      console.log('reverted to original list')
-      return;
-    }
-
-    setisLoading(true);
-    getSearchedSubscriber();
-    setisLoading(false);
-
-  }, [searchQuery]);
+  const {handleClaimPackage, handleLoadMoreSubs } = SubscriberListHook({searchQuery, setisLoading, displaySubsList, 
+    setdisplaySubsList, setnoMoreSubs});
 
   
-
-  const getSearchedSubscriber = async () => {
-    const searchResults : DocumentData[] = [];
-
-
-    const searchQueryID = query(collection(db, 'Subscribers'), orderBy('FIRSTNAME'),
-    where('IDNUMBER', '==', props.searchQuery));
-    const resultsID = await getDocs(searchQueryID)
-
-    resultsID.forEach((result) => {
-      searchResults.push({...result.data(), key: result.id})
-    });
-
-    setsearchResultsList([...searchResults]);
-    // Sets the display list to be only the subscriber that was searched
-  }
-
- const fetchSubscribers = async () => {
-  setisLoading(true);
-  const subs = await getSubscribers();
-  setdisplaySubsList(subs);
-  setinitialSubsList(subs);
-  setisLoading(false);
- }
-
- const handleLoadMoreSubs = async () =>{
-  console.log('Displaying more subscribers');
-  setisLoading(true);
-  const moreSubs = await getMoreSubscribers();
-  if (moreSubs === undefined){
-    setisLoading(false);
-    return;
-  }
-  // Adds the array of newly fetched subscribers to the display list of subscribers
-  setdisplaySubsList((newSubsList) => [...newSubsList, ...moreSubs]);
-  setisLoading(false);
- }
-
- const getSubscribers = async () =>{
-  const subscribers:DocumentData[] = []
-        const q = query(collection(db, 'Subscribers'), orderBy('LASTNAME'), limit(10));
-        const querySnapshot = await getDocs(q);
-        const lastSubDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
-        setlastDoc(lastSubDoc);
-        querySnapshot.forEach((doc)=>{
-            subscribers.push({...doc.data(), key: doc.id})
-        })
-            return subscribers;
-
- }
-
- const getMoreSubscribers = async () =>{
-      const moreSubscribers: DocumentData[] = []
-            
-      const moreSubsQuery = query(collection(db, 'Subscribers'),
-      orderBy('LASTNAME'), startAfter(lastDoc), limit(10));
-
-      const moreSubsList = await getDocs(moreSubsQuery);
-      const lastSubDoc = moreSubsList.docs[moreSubsList.docs.length - 1];
-      setlastDoc(lastSubDoc);
-      if (moreSubsList.size === 0) {
-        setnoMoreSubs(true);
-        console.log("No more documents to fetch.")
-        return;
-      }
-      moreSubsList.forEach((doc)=>{
-          moreSubscribers.push({...doc.data(), key: doc.id})
-      })
-      
-      return moreSubscribers;
- }
-
- const handleClaimPackage = async (id: string, lastName:string, firstName:string) => {
-  Swal.fire({
-   title: 'Mark this package as claimed?',
-   html: '<p>Are you sure you want to mark <strong>' + firstName + ', ' 
-   + lastName + '\'s </strong> package as <strong>claimed?</strong></p>',
-   icon: 'question',
-   showConfirmButton: true,
-   confirmButtonText: 'Yes',
-   showCancelButton: true
-  }).then(async (result)=>{
-    if (result.isConfirmed){
-      const docRef = await setDoc(doc(db, "Subscribers", id), {
-        HASCLAIMED: 'yes',
-        CLAIMDATE: serverTimestamp()
-      }, {merge: true}).then(()=>{
-        Swal.fire(
-          'Success!',
-          'Subscriber info has been successfully updated.',
-          'success'
-        ).then(()=>{
-          getSearchedSubscriber();
-        });
-      }).catch((error)=>{
-        Swal.fire(
-          'Error!',
-          error.message,
-          'error'
-        )
-      })
-    }
-  })
- }
 
  if (isLoading === true) {
   return(
---
+++ src/screens/authenticated/arthur/sub-components/subscriber-list/subscriberListHooks/index.tsx
@@ -0,0 +1,159 @@
+import { collection, doc, DocumentData, getDocs, limit, orderBy, query, serverTimestamp, setDoc, startAfter, where } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import Swal from 'sweetalert2';
+import { db } from '../../../../../../services/firebase-config';
+
+interface SubscriberListHooksInterface{
+    searchQuery: string;
+    setisLoading: React.Dispatch<React.SetStateAction<boolean>>
+    displaySubsList: DocumentData[]
+    setdisplaySubsList:React.Dispatch<React.SetStateAction<DocumentData[]>>
+    setnoMoreSubs:React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const SubscriberListHook = (props: SubscriberListHooksInterface) => {
+  const [lastDoc, setlastDoc] = useState<DocumentData>();
+  const [searchResultsList, setsearchResultsList] = useState<DocumentData[]>([]);
+  const [initialSubsList, setinitialSubsList] = useState<DocumentData[]>([]);
+
+
+  useEffect(() => {
+    fetchSubscribers();
+  }, []);
+
+  useEffect(() => {
+    console.log('search: ');
+    console.log(searchResultsList);
+    props.setdisplaySubsList(searchResultsList);
+  }, [searchResultsList]);
+
+  useEffect(() => {
+    console.log('subsList: ')
+    console.log(props.displaySubsList);
+    console.log('temp list: ')
+  },[props.displaySubsList]);
+
+  useEffect(() => {
+    console.log('query is: ' + props.searchQuery);
+
+    if (props.searchQuery === ""){
+      props.setdisplaySubsList(initialSubsList);
+      console.log('reverted to original list')
+      return;
+    }
+
+    props.setisLoading(true);
+    getSearchedSubscriber();
+    props.setisLoading(false);
+
+  }, [props.searchQuery]);
+
+  
+
+  const getSearchedSubscriber = async () => {
+    const searchResults : DocumentData[] = [];
+
+
+    const searchQueryID = query(collection(db, 'Subscribers'), orderBy('FIRSTNAME'),
+    where('IDNUMBER', '==', props.searchQuery));
+    const resultsID = await getDocs(searchQueryID)
+
+    resultsID.forEach((result) => {
+      searchResults.push({...result.data(), key: result.id})
+    });
+
+    setsearchResultsList([...searchResults]);
+    // Sets the display list to be only the subscriber that was searched
+  }
+
+ const fetchSubscribers = async () => {
+  props.setisLoading(true);
+  const subs = await getSubscribers();
+  props.setdisplaySubsList(subs);
+  setinitialSubsList(subs);
+  props.setisLoading(false);
+ }
+
+ const handleLoadMoreSubs = async () =>{
+  console.log('Displaying more subscribers');
+  props.setisLoading(true);
+  const moreSubs = await getMoreSubscribers();
+  if (moreSubs === undefined){
+    props.setisLoading(false);
+    return;
+  }
+  // Adds the array of newly fetched subscribers to the display list of subscribers
+  props.setdisplaySubsList((newSubsList) => [...newSubsList, ...moreSubs]);
+  props.setisLoading(false);
+ }
+
+ const getSubscribers = async () =>{
+  const subscribers:DocumentData[] = []
+        const q = query(collection(db, 'Subscribers'), orderBy('LASTNAME'), limit(10));
+        const querySnapshot = await getDocs(q);
+        const lastSubDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
+        setlastDoc(lastSubDoc);
+        querySnapshot.forEach((doc)=>{
+            subscribers.push({...doc.data(), key: doc.id})
+        })
+            return subscribers;
+
+ }
+
+ const getMoreSubscribers = async () =>{
+      const moreSubscribers: DocumentData[] = []
+            
+      const moreSubsQuery = query(collection(db, 'Subscribers'),
+      orderBy('LASTNAME'), startAfter(lastDoc), limit(10));
+
+      const moreSubsList = await getDocs(moreSubsQuery);
+      const lastSubDoc = moreSubsList.docs[moreSubsList.docs.length - 1];
+      setlastDoc(lastSubDoc);
+      if (moreSubsList.size === 0) {
+        props.setnoMoreSubs(true);
+        console.log("No more documents to fetch.")
+        return;
+      }
+      moreSubsList.forEach((doc)=>{
+          moreSubscribers.push({...doc.data(), key: doc.id})
+      })
+      
+      return moreSubscribers;
+ }
+
+ const handleClaimPackage = async (id: string, lastName:string, firstName:string) => {
+  Swal.fire({
+   title: 'Mark this package as claimed?',
+   html: '<p>Are you sure you want to mark <strong>' + firstName + ', ' 
+   + lastName + '\'s </strong> package as <strong>claimed?</strong></p>',
+   icon: 'question',
+   showConfirmButton: true,
+   confirmButtonText: 'Yes',
+   showCancelButton: true
+  }).then(async (result)=>{
+    if (result.isConfirmed){
+      const docRef = await setDoc(doc(db, "Subscribers", id), {
+        HASCLAIMED: 'yes',
+        CLAIMDATE: serverTimestamp()
+      }, {merge: true}).then(()=>{
+        Swal.fire(
+          'Success!',
+          'Subscriber info has been successfully updated.',
+          'success'
+        ).then(()=>{
+          getSearchedSubscriber();
+        });
+      }).catch((error)=>{
+        Swal.fire(
+          'Error!',
+          error.message,
+          'error'
+        )
+      })
+    }
+  })
+ }
+  return {handleClaimPackage, handleLoadMoreSubs}
+}
+
+export default SubscriberListHook
